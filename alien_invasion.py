4bdd0cf1bbfa4028fc2a34fc8d7cf211e8e2500b
robbiecares <robbiecares@gmail.com>
2020-07-13 15:55:37 +0200

    bugs with fleet movement corrected

--- alien_invasion.py
+++ alien_invasion.py
@@ -35,10 +35,10 @@ class AlienInvasion:
         """Start the main loop for the game."""
         while True:
             self._check_events()
-            self.ship.update()
             self._update_bullets()
+            self._update_aliens()
+            self.ship.update()
             self._update_screen()
-
 
     def _check_events(self):
         """Respond to keypresses and mouse events."""
@@ -117,6 +117,24 @@ class AlienInvasion:
         alien.rect.x = alien.x
         alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
+
+    def _check_fleet_edges(self):
+        """Respond appropriately if any aliens have reached an edge"""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        """Drop the entire fleet and change the fleet's direction"""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
+
+    def _update_aliens(self):
+        """Check if the fleet is at an edge, then update the positions of all aliens in the fleet"""
+        self._check_fleet_edges()
+        self.aliens.update()
 
     def _update_screen(self):
         """Redraw the screen during each pass through the loop"""
