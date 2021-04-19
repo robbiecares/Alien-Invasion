ae7e2eae335248c54038acb5f801ea12b1824302
robbiecares <robbiecares@gmail.com>
2021-04-19 11:59:58 +0200

    updated .gitignore
    removed base Python directories that should not have been under VCS.

--- scoreboard.py
+++ scoreboard.py
@@ -58,8 +58,9 @@ class Scoreboard:
 
     def show_score(self):
         """Draw scores & level to the screen."""
-        self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.high_score_image, self.high_score_rect)
+        if self.stats.score < self.stats.current_high_score:
+            self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.level_image, self.level_rect)
         self.ships.draw(self.screen)
 
